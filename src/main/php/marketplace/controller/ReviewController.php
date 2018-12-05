db16f12d4962203950334dc0a11e62c33d822937
Ayaz <khn.ayzahmd@gmail.com>
2018-12-05 06:41:29 -0800

    added service and controller layer

---
+++ src/main/php/marketplace/controller/ReviewController.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kayaz
+ * Date: 12/5/2018
+ * Time: 6:35 AM
+ */
+
+class ReviewController
+{
+
+}
---
+++ src/main/php/marketplace/service/CompanyService.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kayaz
+ * Date: 12/5/2018
+ * Time: 6:38 AM
+ */
+
+class CompanyService
+{
+
+}
